3c268b2531ba51f97025f7a61785985d13a05271
hiramrodriguez14 <hiram.rodriguez14@upr.edu>
2026-03-31 00:51:10 -0400

    Extended testing on main file on diagnostics register from ina

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdint.h>
+#define DELAY (1600000) //5ms delay
 
 // ================= INA CONFIG =================
 ina229_t ina = {
@@ -27,6 +28,8 @@ float power = 0.0f;
 float energy = 0.0f;
 float charge = 0.0f;
 
+uint16_t diagnostics = 0;
+uint8_t flags = 0;
 // ================= UART =================
 void uart_send_char(char c) {
     while (DL_UART_Main_isBusy(UART_0_INST)) {
@@ -59,7 +62,16 @@ void uart_printf(const char *fmt, ...) {
         uart_send_char(buffer[i]);
     }
 }
+// ================= INTERRUPTS =================
+void GROUP1_IRQHandler(void){
+    uint32_t status = DL_GPIO_getEnabledInterruptStatus(GPIOB, ALERT_INA229_ALERT_PIN);
+    if(status & ALERT_INA229_ALERT_PIN){
+        delay_cycles(DELAY);
+        DL_GPIO_clearInterruptStatus(GPIOB, ALERT_INA229_ALERT_PIN);
 
+        flags = 1;
+    }
+}
 // ================= MAIN =================
 int main(void)
 {
@@ -79,7 +91,7 @@ int main(void)
     if(ina229_write_shunt_temperature_coefficient(&ina, 0x005A) != INA229_OK){
         uart_printf("INA229 shunt coefficient config register failed\r\n");
         while(1);
-    }   
+    }
 
     //Init
     if (ina229_init(&ina) != INA229_OK) {
@@ -99,9 +111,29 @@ int main(void)
     delay_cycles(3200000);
 
     // ================= LOOP =================
-    while (1) {
 
-        if (ina229_read_shunt_voltage(&ina, &shunt_voltage) == INA229_OK &&
+while(1){
+    if(flags){
+
+       ina229_read_flags(&ina, &diagnostics);
+
+       if(diagnostics & (1 << 7)){
+         uart_printf("Temperature limit exceeded\r\n");
+       }
+       if(diagnostics & (1 << 6)){
+         uart_printf("Shunt voltage upper limit exceeded\r\n");
+       }
+       if(diagnostics & (1 << 5)){
+         uart_printf("Shunt voltage lower limit exceeded\r\n");
+       }
+       if(diagnostics & (1 << 4)){
+         uart_printf("Bus voltage upper limit exceeded\r\n");
+       }
+       if(diagnostics & (1 << 3)){
+         uart_printf("Bus voltage lower limit exceeded\r\n");
+       }
+       if(diagnostics & (1 << 1)){
+         if (ina229_read_shunt_voltage(&ina, &shunt_voltage) == INA229_OK &&
             ina229_read_bus_voltage(&ina, &bus_voltage) == INA229_OK &&
             ina229_read_die_temperature(&ina, &die_temp) == INA229_OK &&
             ina229_read_current(&ina, &current) == INA229_OK &&
@@ -119,7 +151,12 @@ int main(void)
         } else {
             uart_printf("Read error\r\n");
         }
-
-        delay_cycles(32000000);
+       }
+       flags = 0;
     }
+    __disable_irq();   // deshabilita globalmente
+    delay_cycles(32000000);
+    __enable_irq();    // habilita globalmente
+    __WFI(); //Enter LPM and wait for interrupts
+}
 }
